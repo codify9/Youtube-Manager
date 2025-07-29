35038612d25e80d4240a02b244362d59516727b7
codify9 <noobcoderhu@gmail.com>
2025-07-29 15:15:22 +0530

    Fixed some bugs

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,25 +1,25 @@
 import json
 
+
 def load_data():
     try:
         with open('youtube.txt', 'r') as file:
-            test = json.load(file)
-            return test
-    except FileNotFoundError:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
         return []
-    
+
+
 def save_data_helper(videos):
     with open('youtube.txt', 'w') as file:
-        return json.dump(videos, file)
+        json.dump(videos, file)
+
 
 def list_all_videos(videos):
-    print("\n")
-    print('*' * 50)
+    print("\n" + "*" * 50)
     for index, video in enumerate(videos, start=1):
-        print(f"{index}. {video['name']}, Duration: {video['time']}") 
-    
-    print("\n")
-    print('*' * 50)
+        print(f"{index}. {video['name']}, Duration: {video['time']}")
+    print("*" * 50 + "\n")
+
 
 def add_videos(videos):
     name = input("Enter video name: ")
@@ -27,26 +27,34 @@ def add_videos(videos):
     videos.append({'name': name, 'time': time})
     save_data_helper(videos)
 
+
 def update_videos(videos):
     list_all_videos(videos)
-    index = int(input("Enter the video number to be updated: "))
-    if 1<= index <= len(videos):
-        name = input("Enter the new name of video: ")
-        time = input("Enter the duration of video: ")
-        videos[index-1] = {'name': name, 'time': time}
-        save_data_helper(videos)
-    else:
-        print("Invalid Video index!")
+    try:
+        index = int(input("Enter the video number to be updated: "))
+        if 1 <= index <= len(videos):
+            name = input("Enter the new name of video: ")
+            time = input("Enter the duration of video: ")
+            videos[index - 1] = {'name': name, 'time': time}
+            save_data_helper(videos)
+        else:
+            print("Invalid video index!")
+    except ValueError:
+        print("Please enter a valid number.")
 
 
 def delete_videos(videos):
     list_all_videos(videos)
-    index = int(input("Enter the video index to be deleted: "))
-    if 1<= index <= len(videos):
-        del videos[index - 1]
-        save_data_helper(videos)
-    else:
-        print("Invalid video index!")
+    try:
+        index = int(input("Enter the video index to be deleted: "))
+        if 1 <= index <= len(videos):
+            del videos[index - 1]
+            save_data_helper(videos)
+        else:
+            print("Invalid video index!")
+    except ValueError:
+        print("Please enter a valid number.")
+
 
 def main():
     videos = load_data()
@@ -58,30 +66,21 @@ def main():
         print("4. Delete a youtube video.")
         print("5. Exit the application.")
         choice = input("Enter your choice: ")
-        
 
         match choice:
             case '1':
                 list_all_videos(videos)
-            
             case '2':
                 add_videos(videos)
-            
             case '3':
                 update_videos(videos)
-            
             case '4':
                 delete_videos(videos)
-            
             case '5':
                 break
-            
             case _:
-                print("Invalid Number | Please try again")
+                print("Invalid choice | Please try again.")
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
